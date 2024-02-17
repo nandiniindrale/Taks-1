cf0795b0a21f995d0ea9860892daa5f4aa833e76
Nandini <nandiniindrale2002@gmail.com>
2024-02-17 12:40:43 +0530

    Your commit message

---
+++ index.js
@@ -0,0 +1,110 @@
+const express=require("express"); //framework of nodejs which helps to simplify application process
+const mongoose=require("mongoose");
+const bodyParser=require("body-parser") ; //data which comes from client to server is complex so it helps by converting in readable form
+const dotenv= require("dotenv"); //
+
+const app=express();  //express instant
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const username = process.env.MONGODB_USERNAME;
+
+const password = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.xur18xk.mongodb.net/Registrationform`,
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+const registrationSchema = new mongoose.Schema({
+
+    name: String,
+    
+    email: String,
+    
+    password: String
+    
+    });
+
+
+const Registration = mongoose.model("Registration", registrationSchema);
+
+app.use(bodyParser.urlencoded ({extended: true })); 
+
+app.use(bodyParser.json());
+
+
+
+app.get("/",(req,res)=>
+{
+    res.sendFile(__dirname + "/frontened/index.html");
+})
+
+app.post("/register",async(req,res)=>
+{
+    try{
+        const {name,email,password}=req.body;
+        const existingUser = await Registration.findOne({ email: email });
+
+        // check for existing user
+        
+        if (!existingUser) {
+        
+        const registrationData = new Registration({ 
+        name,
+        
+        email,
+        
+        password,
+        
+        });
+        
+        await registrationData.save();
+        
+        res.redirect("/success");
+        
+        }
+        
+        else{
+        
+        alert("User already exist");
+        res.redirect("/error");
+        
+        }
+        const registrationData = new Registration({
+
+            name,
+            
+            email,
+            
+            password
+            
+            });
+            
+           await registrationData.save();
+           res.redirect("/success");
+    }
+    catch(error){
+        console.log("error");
+         res.redirect("error");
+    }
+})
+
+app.get("/success", (req, res)=>{
+
+    res.sendFile (__dirname+"/frontened/success.html");
+    
+    })
+    
+    app.get("/error", (req, res)=>{
+    
+    res.sendFile (__dirname+"/frontened/error.html");
+    })
+
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
